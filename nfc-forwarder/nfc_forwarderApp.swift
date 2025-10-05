0898334e41d21acd770f38c0958ebde7e0caed8f
topper3418 <67675579+topper3418@users.noreply.github.com>
2025-10-05 18:20:56 -0500

    neeed my dev account to go through before this works

---
+++ nfc-forwarder/nfc_forwarderApp.swift
@@ -0,0 +1,17 @@
+//
+//  nfc_forwarderApp.swift
+//  nfc-forwarder
+//
+//  Created by Travis Opperud on 10/5/25.
+//
+
+import SwiftUI
+
+@main
+struct nfc_forwarderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ nfc-forwarderTests/nfc_forwarderTests.swift
@@ -0,0 +1,17 @@
+//
+//  nfc_forwarderTests.swift
+//  nfc-forwarderTests
+//
+//  Created by Travis Opperud on 10/5/25.
+//
+
+import Testing
+@testable import nfc_forwarder
+
+struct nfc_forwarderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
